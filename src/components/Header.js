d825ca041ddd37e757022d866f02a97d7ebdefb7
sagar-7227 <sagarvashnav72427@gmail.com>
2023-11-18 17:25:16 +0530

    use of react router without styling
    Signed-off-by: sagar-7227 <sagarvashnav72427@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <nav>Get Ready to Complete Your Daily goals</nav>;
+  return (
+    <>
+      <nav>Get Ready to Complete Your Daily goals</nav>;
+      <div className="header">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/user/:id">User</Link>
+
+      </div>
+    </>
+  );
 };
 
 export default Header;
---
+++ src/components/User.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const User = () => {
+  const params = useParams();
+  const navigation = useNavigate();
+  console.log(params.id);
+
+  return (
+    <div className='User'>User
+    <button onClick={() => navigation("about")}>CLICK ME</button>
+    </div>
+  )
+
+}
+export default User;
